53019ee8325ce22f7ea4fb834f7c9075eed55c86
Gastón <gastibarossi@gmail.com>
2025-02-24 12:54:46 -0300

    chore: migrated dynamic form from chakra to shadcn

---
+++ components/ui/form/components/error-message.tsx
@@ -0,0 +1,45 @@
+import { FormikTouched } from 'formik';
+import { CircleAlert } from 'lucide-react';
+
+export const ErrorMessage = ({
+  errors,
+  id,
+  touched,
+  key,
+  index,
+}: {
+  errors: any;
+  id: string;
+  touched: FormikTouched<any>;
+  index?: number;
+  key?: string;
+}) => {
+  const errorValue = errors[id];
+  const touchedValue = touched[id];
+  if (!errorValue || !touchedValue) return null;
+
+  const getErrorText = (): string | null => {
+    if (typeof errorValue === 'string') return errorValue;
+
+    if (Array.isArray(errorValue) && index !== undefined) {
+      if (errorValue[index] === undefined) return null;
+
+      if (typeof errorValue[index] === 'string') return errorValue[index];
+      if (typeof errorValue[index] === 'object' && key)
+        return errorValue[index][key] || null;
+    }
+
+    return null;
+  };
+
+  const errorText = getErrorText();
+
+  if (!errorText) return null;
+
+  return (
+    <div className="text-destructive dark:text-destructive flex items-center gap-1 mt-1 text-sm">
+      <CircleAlert className="text-destructive w-4 h-4" />
+      <span>{errorText}</span>
+    </div>
+  );
+};
