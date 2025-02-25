fbb500ecfd206a4d436290f00a63adfab3b4a538
Gastón <gastibarossi@gmail.com>
2025-02-24 22:14:32 -0300

    chore: cleaned up unused files

--- app/layout.tsx
+++ app/layout.tsx
@@ -55,7 +55,7 @@ export default function RootLayout({
                 {children}
               </div>
               <Suspense>
-                <Toaster expand={false} richColors closeButton />
+                <Toaster expand={false} closeButton />
               </Suspense>
               <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
                 <p>
