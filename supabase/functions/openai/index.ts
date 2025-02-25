e6f99c4ce64b76251e1fc908b97c81d612488a09
Gastón <gastibarossi@gmail.com>
2025-02-25 20:02:12 -0300

    chore: removed hardcoded apikey from openai edge function

--- supabase/functions/openai/index.ts
+++ supabase/functions/openai/index.ts
@@ -1,29 +1,28 @@
-import "jsr:@supabase/functions-js/edge-runtime.d.ts";
-import { corsHeaders } from "../_shared/cors.ts";
-import { GPTResponseBody } from "../_shared/types.ts";
-import { getErrorMessage } from "../_shared/helpers.ts";
+import 'jsr:@supabase/functions-js/edge-runtime.d.ts';
+import { corsHeaders } from '../_shared/cors.ts';
+import { GPTResponseBody } from '../_shared/types.ts';
+import { getErrorMessage } from '../_shared/helpers.ts';
 // const OPENAI_API_KEY = Deno.env.get("OPENAI_API_KEY");
-import axios from "npm:axios";
+import axios from 'npm:axios';
 
-const OPENAI_API_KEY =
-  "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";
+const OPENAI_API_KEY = Deno.env.get('OPENAI_API_KEY');
 Deno.serve(async (req) => {
-  if (req.method === "OPTIONS") {
-    return new Response("ok", { headers: corsHeaders });
+  if (req.method === 'OPTIONS') {
+    return new Response('ok', { headers: corsHeaders });
   }
 
   try {
     const body: GPTResponseBody = await req.json();
     const { data } = await axios.post(
-      "https://api.openai.com/v1/chat/completions",
+      'https://api.openai.com/v1/chat/completions',
       body,
       {
         headers: {
           Authorization: `Bearer ${OPENAI_API_KEY}`,
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
         timeout: 240000,
-      },
+      }
     );
     return new Response(
       JSON.stringify({
@@ -31,22 +30,22 @@ Deno.serve(async (req) => {
         data,
       }),
       {
-        headers: { ...corsHeaders, "Content-Type": "application/json" },
+        headers: { ...corsHeaders, 'Content-Type': 'application/json' },
         status: 200,
-      },
+      }
     );
   } catch (error) {
     const errorMessage = getErrorMessage(error);
-    console.error("Error:", errorMessage);
+    console.error('Error while calling OpenAI:', errorMessage);
     return new Response(
       JSON.stringify({
         error:
-          "There was an error while processing your request. Please try again later.",
+          'There was an error while processing your request. Please try again later.',
       }),
       {
-        headers: { ...corsHeaders, "Content-Type": "application/json" },
+        headers: { ...corsHeaders, 'Content-Type': 'application/json' },
         status: 400,
-      },
+      }
     );
   }
 });
