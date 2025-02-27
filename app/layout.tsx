0e3fbe18b12f3f7d5c63efbfe8df1b29d26c6b37
Gastón <gastibarossi@gmail.com>
2025-02-27 18:28:31 -0300

    chore: moved theme switch

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: 'Next.js and Supabase Starter Kit',
+  title: 'Spark & Co',
   description: 'The fastest way to build apps with Next.js and Supabase',
 };
 
@@ -38,9 +38,9 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <main className="min-h-screen flex flex-col items-center">
+          <main className="min-h-screen flex flex-col items-center relative">
             <div className="flex-1 w-full flex flex-col  items-center">
-              <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+              {/* <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
                 <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
                   <div className="flex gap-5 items-center font-semibold">
                     <Link href={'/'}>Next.js Supabase Starter</Link>
@@ -50,14 +50,16 @@ export default function RootLayout({
                   </div>
                   {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
                 </div>
-              </nav>
-              <div className="flex flex-col  container p-5 w-full">
-                {children}
+              </nav> */}
+              <div className="fixed bottom-3 left-3 z-10">
+                <ThemeSwitcher />
               </div>
+              <div className="flex flex-col  container  w-full">{children}</div>
+
               <Suspense>
                 <Toaster expand={false} closeButton />
               </Suspense>
-              <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
+              {/* <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
                 <p>
                   Powered by{' '}
                   <a
@@ -69,8 +71,7 @@ export default function RootLayout({
                     Supabase
                   </a>
                 </p>
-                <ThemeSwitcher />
-              </footer>
+              </footer> */}
             </div>
           </main>
         </ThemeProvider>
