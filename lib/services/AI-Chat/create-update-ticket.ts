65fb644cf7dea6ef039c071d5e38f3bfd0f3402e
Gastón <gastibarossi@gmail.com>
2025-03-07 16:38:29 -0300

    refact: issueType now being properly assigned to new tickets

--- lib/services/AI-Chat/create-update-ticket.ts
+++ lib/services/AI-Chat/create-update-ticket.ts
@@ -1,17 +1,16 @@
 'use server';
 import { UpdateReponseType } from '@/lib/types/utils/functions-return-type';
 import { parseFormData } from '@/utils/helpers';
-import { getJiraDefaultValues } from '../jira/get-jira-default-values'; // Mantén esta importación
+import { getJiraDefaultValues } from '../jira/get-jira-default-values';
 import { supabase } from '@/lib/supabase/client';
 
 export const createOrUpdateTicket = async (
   formData: FormData
 ): Promise<UpdateReponseType> => {
   const values = parseFormData(formData, 'values');
-  const { id, summary, description, priority } = values;
+  const { id, summary, description, priority, issueType } = values;
 
   const method = id ? 'PUT' : 'POST';
-
   const body = getJiraDefaultValues({
     method,
     endpoint: id ? `rest/api/3/issue/${id}` : 'rest/api/3/issue',
@@ -19,13 +18,13 @@ export const createOrUpdateTicket = async (
     description,
     priority,
     projectKey: 'TEST',
-    issueType: 'Task',
+    issueType,
   });
-  // Llamada a la función de Supabase para invocar la función de Jira
   const { data: jiraData, error: jiraError } = await supabase.functions.invoke(
     'jira',
     { body }
   );
+
   if (jiraError) {
     console.log(jiraError);
     return {
