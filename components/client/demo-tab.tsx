e73d04460cca2cf3c2f2aa3237eb78d58dd1abc3
Santiago <santiagonaguero@gmail.com>
2026-09-04 17:05:09 -0300

    SPA-466

--- components/client/demo-tab.tsx
+++ components/client/demo-tab.tsx
@@ -19,7 +19,7 @@ import { useUser } from "context/UserContext";
 import { API_HEADERS, API_JSON_HEADERS } from "@/lib/api-headers";
 import { type Demo, type DemoUser, isImageFile, getEmbedIframeSrc, displayName } from "@/lib/demo-video-utils";
 import { DemoPicker } from "@/components/developer/demo-picker";
-import { getIssueCode, deriveIssueKind } from "@/lib/utils";
+import { PreviewLinksBanner } from "./preview-links-banner";
 import type { Issue } from "./issues.types";
 
 type DemoComment = {
@@ -380,6 +380,8 @@ export function DemoTab({ issue, slug }: { issue: Issue; slug?: string }) {
           Upload or embed demo videos and images for this issue and gather feedback
         </p>
       </div>
+
+      <PreviewLinksBanner slug={slug} />
 
       {/* Add or update a version */}
       <div className="flex flex-wrap items-end gap-3">
