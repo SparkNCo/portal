e109958034363912e767b30feed1f927c3236e54
Santiago <santiagonaguero@gmail.com>
2026-09-14 18:40:04 -0300

    small screen styles in Bugs and Build Dashboards

--- app/[slug]/(portal)/build/page.tsx
+++ app/[slug]/(portal)/build/page.tsx
@@ -183,7 +183,7 @@ function BuildPageContent() {
         )}
 
         <div className="-mx-4 sm:mx-0 grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-6">
-          <div className="relative flex flex-col">
+          <div className="relative">
             <PinButton panelId="build_product_decisions" />
             <div className="pt-12 flex-1">
               <PriorityTasks
